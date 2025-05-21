451642a2e6b06c38247ebf9b4d21edf0a059380c
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-21 17:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function volume_sphere() {
     //Write your code here
+	const r = parseFloat(document.getElementById("radius").value)
+    const voummeField = document.getElementById("voumme")
+    if(NaN(r) || r < 0){
+        voummeField.value = "NaN"
+        return false
+    }
+    const volume = (4/3) * Math.PI * Math.pow(r,3)
+    voummeField.value = volume.toFixed(4)
+
+	return false
   
 } 
 
